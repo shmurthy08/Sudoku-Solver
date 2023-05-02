85fa59c2d65c4a179ee6c229d95571a33c2cabfd
shmurthy08 <shmurthy@chapman.edu>
2023-05-02 12:25:08 -0700

    Added solver 2.py

---
+++ Generator.py
@@ -0,0 +1,17 @@
+import requests
+from tabulate import tabulate
+
+
+response = requests.get('https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,solution,difficulty}}}')
+data = response.json()
+difficulty = data['newboard']['grids'][0]['difficulty']
+board_data = data['newboard']['grids'][0]['value']
+
+board = [['.' if cell == 0 else str(cell) for cell in row] for row in board_data]
+table = tabulate(board, tablefmt='plain')
+
+
+#write board into a .txt file
+with open('sudoku_board.txt', 'w') as f:
+    for row in board:
+        f.write(''.join(row) + '\n')
